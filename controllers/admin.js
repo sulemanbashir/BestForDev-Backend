cf94b2590801ceff0d8afde22009969120f28946
Suleman Bashir <sulemanbashir02@hotmail.com>
2019-02-24 16:56:28 +0500

    API Documentation only return for User

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,14 +2,14 @@
 const db = require('../database/db-connection')
 
 module.exports = function(admin_app) {
-    admin_app.get('/', getAdminRoot)
+    admin_app.get('/admin', getAdminRoot)
     admin_app.get('/link/proposed', getProposed)
     admin_app.put('/link/approve', putApprove)
     admin_app.delete('/link/delete', deleteLink)
 }
 
 function getAdminRoot(req, res) {
-    res.sendFile('/Endpoints.html', { root: '.' })
+    res.status(200).send(`Inside Admin`)
 }
 
 function getProposed(req, res) {
